a2a06ba1e6dd29ec3ca07a50a366a9bed821e162
Hatem <gadallah.hatem@gmail.com>
2025-04-28 15:24:39 +0400

    remove jose and add JWT

--- lib/engine/security.py
+++ lib/engine/security.py
@@ -1,17 +1,18 @@
 import logging
+import os
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
+import jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
-from datetime import datetime, timedelta
-from lib.schemas.user import TokenData, User
+from pydantic import ValidationError
 from sqlalchemy.orm import Session
+
 from config import get_db
 from lib.models.User import User as SQLUser
+from lib.schemas.user import TokenData, User
 from passlib.context import CryptContext
-from pydantic import ValidationError
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -30,33 +31,35 @@ def get_password_hash(password):
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+    expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+def decode_access_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except (JWTError, ValidationError):
-        raise credentials_exception
-    user = db.query(SQLUser).filter(SQLUser.username == token_data.name).first()
-    try:
-        if user is None:
-            raise credentials_exception
-        return user
-    except Exception as e:
-        logging.error(e)
+        return payload
+    except jwt.JWTError:
         return None
 
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    payload = decode_access_token(token)
+    if payload is None:
+        raise credentials_exception
+    username: str = payload.get("sub")
+    if username is None:
+        raise credentials_exception
+    token_data = TokenData(username=username)
+    user = db.query(SQLUser).filter(SQLUser.username == token_data.name).first()
+    if user is None:
+        raise credentials_exception
+    return user
 
 async def fake_current_user():
     """Temporary function to bypass authentication."""
