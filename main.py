f8845817d1bc4c11935e5a382cc2dcc9fef2b58a
Hatem <gadallah.hatem@gmail.com>
2025-05-01 07:26:30 +0400

    Update origins again 2

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ origins = ["http://localhost:3000/"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= ["https://tahlyl-frontend.netlify.app/", "https://tahlyl-frontend.netlify.app/"],
+    allow_origins= ["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
