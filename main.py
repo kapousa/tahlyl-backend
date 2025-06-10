275c76f83321207971e8a832ade31873a49b17c4
hatem <gadallah.hatem@gmail.com>
2025-06-10 13:23:24 +0400

    update origin values

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ else:
         "http://localhost:8080",
         "http://127.0.0.1:8080",
     ]
-# --- End Environment Configuration ---
+# --- End Environment Configuration. ---
 
 
 app = FastAPI(
