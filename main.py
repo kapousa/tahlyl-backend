a07c5fd7b07dd09842c51dcb2b53c63133eccd81
hatem <gadallah.hatem@gmail.com>
2025-06-27 13:04:19 +0400

    trying to fix prod issue - log issue

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ app.add_middleware(LogRequestsMiddleware)
 app.add_middleware(AuthenticationMiddleware, backend=JWTAuthBackend())
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= allowed_origins,
+    allow_origins= "https://tahlyl-frontend.netlify.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
